2df0fbf75d7cb67916105bd3c146200f181f8018
Jason Fleet <jason.fleet@googlemail.com>
2022-08-24 12:10:21 +0100

    removed answer button code.

--- components/quiz/answer-buttons.tsx
+++ components/quiz/answer-buttons.tsx
@@ -20,36 +20,38 @@ const Answers = ({ correctAnswer, incorrectAnswers, onAnswer }: AnswersProps) =>
   }
 
 
-  useEffect(() => {
-    if (question !== null) {
-      let ans: Array<Answer> = []
-      let ansIndex = 0
-      let correctAnswerIndex = Math.floor(Math.random() * 4)
+  // useEffect(() => {
+  //   if (question !== null) {
+  //     let ans: Array<string> = []
+  //     let ansIndex = 0
+  //     let correctAnswerIndex = Math.floor(Math.random() * 4)
 
-      setAnswer(null)
-      setHasCorrectAnswer(false)
+  //     // setAnswer(null)
+  //     // setHasCorrectAnswer(false)
 
-      for (let i = 0, j = question.incorrectAnswers.length + 1; i < j; i++) {
-        if (correctAnswerIndex === i) {
-          ans.push({
-            answer: question.correctAnswer,
-            key: String.fromCharCode(65 + i),
-          })
-        } else {
-          ans.push({
-            answer: question.incorrectAnswers[ansIndex++],
-            key: String.fromCharCode(65 + i),
-          })
-        }
-      }
+  //     for (let i = 0, j = question.incorrectAnswers.length + 1; i < j; i++) {
+  //       if (correctAnswerIndex === i) {
+  //         ans.push({
+  //           answer: question.correctAnswer,
+  //           key: String.fromCharCode(65 + i),
+  //         })
+  //       } else {
+  //         ans.push({
+  //           answer: question.incorrectAnswers[ansIndex++],
+  //           key: String.fromCharCode(65 + i),
+  //         })
+  //       }
+  //     }
 
-      setAnswers(ans)
-    }
-  }, [correctAnswer, incorrectAnswers])
+  //     setAnswers(ans)
+  //   }
+  // }, [correctAnswer, incorrectAnswers])
 
   return <div className='flex flex-row gap-4 justify-center mt-8'>
-    {answers && answers.map((id: string, i: number) => {
-      return <button className='quiz-answer-button' key={'quiz-answer-button' + i} onClick={() => onAnswer(id)}>{questionAnswer.key}</button>
+    {incorrectAnswers && incorrectAnswers.map((answer, i) => {
+      return <button className='quiz-answer-button' key={'quiz-answer-button' + i} onClick={() => onAnswer(answer)}>
+        {answer}
+      </button>
     })}
 
   </div>
