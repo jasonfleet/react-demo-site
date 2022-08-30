fa12d1232a4af9bd09c200673c954e6f6b3cb717
Jason Fleet <jason.fleet@googlemail.com>
2022-08-30 20:08:45 +0100

    Rock paper scissors images.

--- pages/games/rock-paper-scissors.tsx
+++ pages/games/rock-paper-scissors.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import Layout from '../../components/layout'
+import paper from '../../public/images/paper.png'
+import scissors from '../../public/images/scissors.png'
+import rock from '../../public/images/rock.png'
 
 function RockPaperScissors() {
-  return <Layout classStyle='quiz'>
+  return <Layout>
     <div className='title'>Rock-Paper-Scissors</div>
+    <div className='grid grid-flow-col grid-cols-3 gap-4'>
+      <div><Image alt='ROCK' height='210px' layout='intrinsic' src={rock} width='210px' /></div>
+      <div><Image alt='PAPER' height='210px' layout='intrinsic' src={paper} width='210px' /></div>
+      <div><Image alt='SCISSORS' height='210px' layout='intrinsic' src={scissors} width='210px' /></div>
+      </div>
   </Layout>
 }
 
