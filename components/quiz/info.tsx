5c43a4f195ae76c5babe13a63d7d72faf618d587
Jason Fleet <jason.fleet@googlemail.com>
2022-09-12 10:40:52 +0100

    Added infoe and a bit of housekeeping for quiz-game-over.

--- components/quiz/info.tsx
+++ components/quiz/info.tsx
@@ -1,13 +1,24 @@
+import Link from "next/link"
 import { useState } from "react"
 
 const Info = () => {
   const [open, setOpen] = useState<boolean>(false)
 
-  return <div className='absolute p-2 top-0 right-0'>
-    <div className='text-right'><button onClick={() => setOpen(!open)} type='button'><h2>Info</h2></button></div>
+  return <div className='info-popup'>
+    <div className='text-right'>
+      <button className='quiz-button' onClick={() => setOpen(!open)} type='button'><h2>Info</h2></button>
+    </div>
     {open &&
-      <div className='border rounded-lg p-2'>
-        some info about something
+      <div className='border rounded-lg p-2 flex flex-col gap-2' onClick={() => setOpen(false)}>
+        <div>
+          The questions are from <Link href='https://the-trivia-api.com'><a target='__blank'>THE TRIVIA API</a></Link>
+        </div>
+        <div>
+          <strong>Note:</strong> catagories have not been implemented.
+        </div>
+        <div className='underline'>
+          Click to close
+        </div>
       </div>
     }
   </div>
