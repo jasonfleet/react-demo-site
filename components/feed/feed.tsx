c966786210e2146be5eb49aa2e9403a1b45aec7b
Jason Fleet <jason.fleet@googlemail.com>
2022-10-28 17:50:07 +0100

    Fixed any type.

--- components/feed/feed.tsx
+++ components/feed/feed.tsx
@@ -1,6 +1,6 @@
 import useSWR from 'swr'
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 const Feed = () => {
   const { data, error } = useSWR('/api/feed', fetcher)
